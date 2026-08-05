d0cfaed736793e909c8c757d428c497ec8d96958
Fhanafii <fhanafi544@gmail.com>
2026-08-05 11:48:32 +0700

    feat:init setup monitoring project

---
+++ monitoring/app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function HomePage() {
+  return (
+    <main className="flex min-h-screen items-center justify-center">
+      <h1 className="text-4xl font-bold">
+        IoT Security Monitoring
+      </h1>
+    </main>
+  )
+}
