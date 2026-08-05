7aa8c3c17fbb0eef37b00e4070d50b4be1f61550
Fhanafii <fhanafi544@gmail.com>
2026-08-06 01:26:02 +0700

    feat: adding exception handle for database connection

--- server/database.py
+++ server/database.py
@@ -92,85 +92,107 @@ class Database:
             return False
 
     def get_all(self):
+        conn=self.connect()
+        try:
 
-        conn = self.connect()
-        with conn.cursor() as cur:
-            cur.execute(
-                """
-                SELECT *
-                FROM detections
-                ORDER BY detected_at DESC
-                """
-            )
+            with conn.cursor() as cur:
+                cur.execute(
+                    """
+                    SELECT *
+                    FROM detections
+                    ORDER BY detected_at DESC
+                    """
+                )
+            return cur.fetchone()
 
-            return cur.fetchall()
+        except Exception:
+            conn.rollback()
+            raise
 
     def get_by_id(self, detection_id):
 
         conn = self.connect()
-        with conn.cursor() as cur:
-            cur.execute(
-                """
-                SELECT *
-                FROM detections
-                WHERE id=%s
-                """,
-                (detection_id,),
-            )
+        try:
 
+            with conn.cursor() as cur:
+
+                cur.execute(
+                    """
+                    SELECT *
+                    FROM detections
+                    WHERE id=%s
+                    """,
+                    (detection_id,),
+                )
             return cur.fetchone()
+        
+        except Exception:
+            conn.rollback()
+            raise
 
 
     def get_by_date(self, start_date, end_date):
 
-        conn = self.connect()
+        conn=self.connect()
 
-        with conn.cursor() as cur:
-            cur.execute(
-                """
-                SELECT *
-                FROM detections
+        try:
 
-                WHERE detected_at
-                BETWEEN %s AND %s
+            with conn.cursor() as cur:
+                cur.execute(
+                    """
+                    SELECT *
+                    FROM detections
 
-                ORDER BY detected_at DESC
-                """,
-                (start_date, end_date),
-            )
+                    WHERE detected_at
+                    BETWEEN %s AND %s
 
-            return cur.fetchall()
-
-    def get_dashboard(self):
-        conn = self.connect()
-
-        with conn.cursor() as cur:
-            cur.execute("""
-                SELECT
-                    COUNT(*) AS total_batch,
-
-                    SUM(detected_frames) AS total_detected_frames,
-
-                    COUNT(*) FILTER (
-                        WHERE status = 'Normal'
-                    ) AS normal,
-
-                    COUNT(*) FILTER (
-                        WHERE status = 'Perlu Dipantau'
-                    ) AS monitoring,
-
-                    COUNT(*) FILTER (
-                        WHERE status = 'Mencurigakan'
-                    ) AS suspicious,
-
-                    COUNT(*) FILTER (
-                        WHERE whatsapp_sent = TRUE
-                    ) AS whatsapp_sent
-
-                FROM detections;
-            """)
+                    ORDER BY detected_at DESC
+                    """,
+                    (start_date, end_date),
+                )
 
             return cur.fetchone()
+
+        except Exception:
+            conn.rollback()
+            raise
+
+    def get_dashboard(self):
+        conn=self.connect()
+
+        try:
+
+            with conn.cursor() as cur:
+                cur.execute("""
+                    SELECT
+                        COUNT(*) AS total_batch,
+
+                        SUM(detected_frames) AS total_detected_frames,
+
+                        COUNT(*) FILTER (
+                            WHERE status = 'Normal'
+                        ) AS normal,
+
+                        COUNT(*) FILTER (
+                            WHERE status = 'Perlu Dipantau'
+                        ) AS monitoring,
+
+                        COUNT(*) FILTER (
+                            WHERE status = 'Mencurigakan'
+                        ) AS suspicious,
+
+                        COUNT(*) FILTER (
+                            WHERE whatsapp_sent = TRUE
+                        ) AS whatsapp_sent
+
+                    FROM detections;
+                """)
+
+            return cur.fetchone()
+        
+        except Exception:
+            conn.rollback()
+            raise
 
     def get_paginated(
         self,
@@ -181,93 +203,99 @@ class Database:
         end=None,
         keyword=None
     ):
-
         conn = self.connect()
-        offset = (page - 1) * limit
+        try:
+            offset = (page - 1) * limit
 
-        query = """
-            SELECT *
-            FROM detections
-            WHERE 1=1
-        """
-
-        params = []
-
-        if status:
-            query += " AND status = %s"
-            params.append(status)
-
-        if start:
-            query += " AND detected_at >= %s"
-            params.append(start)
-
-        if end:
-            query += " AND detected_at <= %s"
-            params.append(end)
-
-        if keyword:
-            query += """
-                AND (
-                    CAST(batch_number AS TEXT) ILIKE %s
-                    OR status ILIKE %s
-                    OR CAST(detected_at AS TEXT) ILIKE %s
-                )
+            query = """
+                SELECT *
+                FROM detections
+                WHERE 1=1
             """
 
-            params.extend([
-                f"%{keyword}%",
-                f"%{keyword}%",
-                f"%{keyword}%"
-            ])
+            params = []
 
-        count_query = f"""
-            SELECT COUNT(*)
-            FROM ({query}) AS filtered
-        """
+            if status:
+                query += " AND status = %s"
+                params.append(status)
 
-        query += """
-            ORDER BY detected_at DESC
-            LIMIT %s OFFSET %s
-        """
+            if start:
+                query += " AND detected_at >= %s"
+                params.append(start)
 
-        with conn.cursor() as cur:
+            if end:
+                query += " AND detected_at <= %s"
+                params.append(end)
 
-            cur.execute(count_query, params)
-            total = cur.fetchone()["count"]
+            if keyword:
+                query += """
+                    AND (
+                        batch_number::TEXT ILIKE %s
+                        OR status ILIKE %s
+                        OR detected_at::TEXT ILIKE %s
+                    )
+                """
 
-            cur.execute(
-                query,
-                params + [limit, offset]
-            )
+                params.extend([
+                    f"%{keyword}%",
+                    f"%{keyword}%",
+                    f"%{keyword}%"
+                ])
 
-            rows = cur.fetchall()
+            count_query = f"""
+                SELECT COUNT(*)
+                FROM ({query}) filtered
+            """
 
-        return {
-            "rows": rows,
-            "total": total,
-            "page": page,
-            "limit": limit,
-            "total_pages": (total + limit - 1) // limit
-        }
+            query += """
+                ORDER BY detected_at DESC
+                LIMIT %s OFFSET %s
+            """
+
+            with conn.cursor() as cur:
+
+                cur.execute(count_query, params)
+                total = cur.fetchone()["count"]
+
+                cur.execute(
+                    query,
+                    params + [limit, offset]
+                )
+
+                rows = cur.fetchall()
+
+            return {
+                "rows": rows,
+                "total": total,
+                "page": page,
+                "limit": limit,
+                "total_pages": (total + limit - 1) // limit
+            }
+
+        except Exception:
+            conn.rollback()
+            raise
 
 
     def get_files_info(self, detection_id):
-        conn = self.get_connection()
-        cur = conn.cursor()
-        cur.execute("""
-            SELECT
-                batch_folder,
-                batch_number
-            FROM detections
-            WHERE id = %s
-        """, (detection_id,))
+        conn=self.connect()
 
-        row = cur.fetchone()
+        try:
 
-        cur.close()
-        conn.close()
+            with conn.cursor() as cur:
+                cur.execute("""
+                    SELECT
+                        batch_folder,
+                        batch_number
+                    FROM detections
+                    WHERE id=%s
+                """,(detection_id,))
+
+            return cur.fetchone()
         
-        return row
+        except Exception:
+            conn.rollback()
+            raise
 
     def delete_detection(self, detection_id):
         conn = self.connect()
